544654c61e99f2bb0e25045fe753d0f8fa94ae6c
milh cbt <milh_cbt@yahoo.com>
2016-10-10 15:21:37 +0700

    demo primitif data type.

--- src/main/java/com/codencare/learning/primitif/PrimitiveTypeDemo.java
+++ src/main/java/com/codencare/learning/primitif/PrimitiveTypeDemo.java
@@ -3,7 +3,29 @@ package com.codencare.learning.primitif;
 public class PrimitiveTypeDemo
 {
 	public static void main(String [] args){
-		int i1 = 2;
-		
+                boolean logikaBenar = true;
+                boolean logikaSalah = false;
+            
+		byte bilanganBulat8BitPositif = 8;
+                byte bilanganBulat8BitNegatif = -8;                
+		short bilanganBulatPendekPositif = 16;
+                short bilanganBulatPendekNegatif = -16;
+                int bilanganBulatPositif = 32;
+                int bilanganBulatNegatif = -32;
+                long bilanganBulatPanjangPositif = 2147483648L;
+                long bilanganBulatPanjangNegtif = -2147483649L;
+                char karakter = 'A';
+                char bilanganPositif = 65;
+                //char bilanganNegatif = -65;//error gak bisa negatif unsigned integer
+                
+                System.out.println("karakter:"+karakter);
+                System.out.println("bilanganPositif:"+bilanganPositif);
+                System.out.println("bilanganPositif (short):"+ (short)bilanganPositif);
+                
+                float bilanganMengambangPresisiTunggal = 3.14285714286F;
+                Double bilanganMengambangPresisiDouble = 22.0/7.0;
+                
+                System.out.println("bilanganMengambangPresisiDouble:"+bilanganMengambangPresisiDouble);
+                System.out.println("bilanganMengambangPresisiTunggal:"+bilanganMengambangPresisiTunggal);   
 	}
 }
