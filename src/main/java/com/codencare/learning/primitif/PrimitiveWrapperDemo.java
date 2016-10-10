fb1866271e325800438abccdb34a12af33a9c393
milh cbt <milh_cbt@yahoo.com>
2016-10-10 15:22:43 +0700

    Primitive Wrapper

---
+++ src/main/java/com/codencare/learning/primitif/PrimitiveWrapperDemo.java
@@ -0,0 +1,15 @@
+package com.codencare.learning.primitif;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author iman
+ */
+public class PrimitiveWrapperDemo {
+    
+}
