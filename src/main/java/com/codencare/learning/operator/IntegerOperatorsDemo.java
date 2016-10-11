12bc0cc63d945e527211fa7995b3ee345d20acc9
milh cbt <milh_cbt@yahoo.com>
2016-10-11 15:00:11 +0700

    add opperator demo

---
+++ src/main/java/com/codencare/learning/operator/IntegerOperatorsDemo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codencare.learning.operator;
+
+/**
+ *
+ * @author iman
+ */
+public class IntegerOperatorsDemo {
+
+    public static void main(String[] args) {
+        int X = 2, Y = 5, Z = 6;
+        int min = Integer.MIN_VALUE, max = Integer.MAX_VALUE;
+
+        System.out.println("X + Y = " + (X + Y));
+        System.out.println("max = " + max);
+        System.out.println("max + 1 = " + (max + 1));
+        System.out.println("min = " + min);
+        System.out.println("min - 1 = " + (min - 1));
+
+        System.out.println("5 / 2 = " + (Y / X));
+        System.out.println("5 % 2 = " + (Y % X));
+        System.out.println("6 / 2 = " + (Z / X));
+        System.out.println("6 % 2 = " + (Z % X));
+        System.out.println("5 x 2 = " + (Y * X));
+    }
+}
--- src/main/java/com/codencare/learning/primitif/PrimitiveWrapperDemo.java
+++ src/main/java/com/codencare/learning/primitif/PrimitiveWrapperDemo.java
@@ -11,5 +11,22 @@ package com.codencare.learning.primitif;
  * @author iman
  */
 public class PrimitiveWrapperDemo {
-    
+    public static void main (String[] args){
+        byte b1Max = Byte.MAX_VALUE;
+        Byte b2Max = Byte.MAX_VALUE;
+        byte b1Min = Byte.MIN_VALUE;
+        Byte b2Min = Byte.MIN_VALUE;
+        
+        System.out.println("b1Max:"+b1Max);
+        System.out.println("b2Max:"+b2Max);
+        System.out.println("b1Min:"+b1Min);
+        System.out.println("b2Min:"+b2Min);
+        
+        int intString = Byte.parseByte("A", 16);
+        int i1 = new Integer(5);///auto unboxing, unnecessary
+        int i2 = 5;//
+        Integer i3 =  5;//auto boxing 
+        int i4 = i3.intValue();
+        long longValue1 = i3.longValue();
+    }
 }
