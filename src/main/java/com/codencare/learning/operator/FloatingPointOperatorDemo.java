ccd737d60960ade94e02785ac1f8c03f4471f016
milh cbt <milh_cbt@yahoo.com>
2016-10-11 15:43:42 +0700

    if then

--- src/main/java/com/codencare/learning/operator/FloatingPointOperatorDemo.java
+++ src/main/java/com/codencare/learning/operator/FloatingPointOperatorDemo.java
@@ -10,5 +10,20 @@ package com.codencare.learning.operator;
  * @author iman
  */
 public class FloatingPointOperatorDemo {
-    
+    public static void main (String [] args){
+        double X = 2.0, Y = 5.0, Z = 6.0;
+        double min = Double.MIN_VALUE, max = Double.MAX_VALUE;
+        
+        System.out.println("X + Y = " + (X + Y));
+        System.out.println("max = " + max);
+        System.out.println("max + 1 = " + (max + 1));
+        System.out.println("min = " + min);
+        System.out.println("min - 1 = " + (min - 1));
+
+        System.out.println("5 / 2 = " + (Y / X));
+        System.out.println("5 % 2 = " + (Y % X));
+        System.out.println("6 / 2 = " + (Z / X));
+        System.out.println("6 % 2 = " + (Z % X));
+        System.out.println("5 x 2 = " + (Y * X));
+    }
 }
