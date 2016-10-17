1fb38aa0a3314492a259b03fa1f675a24410f30e
milh cbt <milh_cbt@yahoo.com>
2016-10-17 20:12:36 +0700

    formating

--- src/main/java/com/codencare/learning/operator/IntegerOperatorsDemo.java
+++ src/main/java/com/codencare/learning/operator/IntegerOperatorsDemo.java
@@ -26,6 +26,7 @@ public class IntegerOperatorsDemo {
         System.out.println("6 / 2 = " + (Z / X));
         System.out.println("6 % 2 = " + (Z % X));
         System.out.println("5 x 2 = " + (Y * X));
+        
         System.out.println("Z +=5 ==> Z = " + (Z += 2));
         System.out.println("Z -=5 ==> Z = " + (Z -= 2));
         System.out.println("Z *=5 ==> Z = " + (Z *= 2));
