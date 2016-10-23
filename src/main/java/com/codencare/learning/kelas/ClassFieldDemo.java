96f62b576598a74548d4ca357053c7fab4c2729b
milh cbt <milh_cbt@yahoo.com>
2016-10-23 16:06:05 +0700

    better sample for exception

--- src/main/java/com/codencare/learning/kelas/ClassFieldDemo.java
+++ src/main/java/com/codencare/learning/kelas/ClassFieldDemo.java
@@ -1,19 +1,18 @@
 package com.codencare.learning.kelas;
 
-public class ClassFieldDemo
-{
-	public static void main(String[] args){ 
-		ClassMember cm = new ClassMember();
-		System.out.println(cm.i);
+public class ClassFieldDemo {
+
+    public static void main(String[] args) {
+        ClassMember cm = new ClassMember();
+        System.out.println(cm.i);
         cm.i = 5;
         System.out.println(cm.i);
-	}
+    }
 }
 
-class ClassMember{
-	int i=2;
-
-	ClassMember(){ 
-		System.out.println("di kontruktor..");
-	}
+class ClassMember {
+    int i = 2;
+    ClassMember() {
+        System.out.println("di kontruktor..");
+    }
 }
