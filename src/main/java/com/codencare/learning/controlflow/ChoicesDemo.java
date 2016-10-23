ca342078c5f15157b805c0fd8d84100c6e22844c
milh cbt <milh_cbt@yahoo.com>
2016-10-23 08:27:35 +0700

    formating for ppt

--- src/main/java/com/codencare/learning/controlflow/ChoicesDemo.java
+++ src/main/java/com/codencare/learning/controlflow/ChoicesDemo.java
@@ -17,10 +17,8 @@ public class ChoicesDemo {
             System.out.println("the truth always win");
         }
         
-        if (salah) {
-            System.out.println("the truth is out there");
-        }
-        
+        if (salah) System.out.println("the truth is out there");
+                
         if (salah) {
             System.out.println("jika tidak salah");
         } else {
@@ -30,20 +28,15 @@ public class ChoicesDemo {
         int x = 5, y = 6;
         String terbilang;
         if(x == 0) terbilang = "Nol";
-        else if (x == 1) terbilang = "Satu";
-        else if (x == 2) terbilang = "Dua";
-        else if (x == 3) terbilang = "Tiga";
-        else if (x == 4) terbilang = "Empat";
-        else if (x == 5) terbilang = "Lima";
-        else if (x == 6) terbilang = "Enam";
-        else if (x == 7) terbilang = "Tujuh";
-        else if (x == 8) terbilang = "Delapan";
+        else if (x == 1) terbilang = "Satu";else if (x == 2) terbilang = "Dua";
+        else if (x == 3) terbilang = "Tiga";else if (x == 4) terbilang = "Empat";
+        else if (x == 5) terbilang = "Lima"; else if (x == 6) terbilang = "Enam";
+        else if (x == 7) terbilang = "Tujuh";else if (x == 8) terbilang = "Delapan";
         else if (x == 9) terbilang = "Sembilan";
         else terbilang = "tak dikenal";
         
         System.out.println("terbilang = "+terbilang);
-		
-	    int bilangTerbesar = x > y? x : y; 
-		System.out.println("bilangTerbesar = " +bilangTerbesar);
-	}
+	int bilangTerbesar = x > y? x : y; 
+	System.out.println("bilangTerbesar = " +bilangTerbesar);
+    }
 }
